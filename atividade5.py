73cef71582448e4490c3ca2e5ec34c0bd9bf8a03
33669933 <vivianmariasantos825@gmail.com>
2023-10-23 10:12:43 -0300

    first commit

---
+++ atividade5.py
@@ -0,0 +1,10 @@
+metros = float(input("digite o valor em metros \n"))
+cm =metros * 100
+mm = metros * 1000
+
+print(f"""Convertendo o valor {metros} M
+      
+      MM = {mm} mm
+      CM = {cm} cm 
+      
+      """)
